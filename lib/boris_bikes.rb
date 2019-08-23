abaaa9221212fb182055b1fcf5d103243d46b70c
Canvasbag989 <Canvasbag989@icloud.com>
2019-08-23 16:12:24 +0100

    Private method added ex 15

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -6,19 +6,27 @@ class DockingStation
     @all_bikes = []
   end
 
-
-
   def release_bike
-    fail "No Bikes available" if @all_bikes.empty?
+    fail "No Bikes available" if empty?
     @all_bikes.pop
   end
 
   def dock_bike(bike)
-    fail "Unable to dock: docking station full" if @all_bikes.count >= 20
+    fail "Unable to dock: docking station full" if full?
     @all_bikes << bike
     bike
   end
 
+  private
+
+  def full?
+
+    @all_bikes.count >= 20
+  end
+end
+
+def empty?
+  @all_bikes.empty?
 end
 
 class Bike
